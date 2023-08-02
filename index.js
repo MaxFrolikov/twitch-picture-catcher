1e293aacd6995a0e588f63d6b1c7a4d80f9a1ed5
MaxFrolikov <37468597+MaxFrolikov@users.noreply.github.com>
2023-08-02 22:32:50 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,45 @@
-console.log('starting')
+console.log('Starting...\n')
+
+const redLog = '\x1b[31m'
+const greenLog = '\x1b[32m'
+
+const customLog = (type, param, text) => {
+	console.log(`${type} | ${(new Date).toLocaleTimeString()} |  ${param} ${text}\x1b[0m`)
+}
 
 const fs = require('fs');
 const tmi = require('tmi.js');
 const https = require ('https');
 const sharp = require('sharp');
 
-console.log('ReadingConfig')
-
+console.log('ReadingConfig...')
 const config = JSON.parse(fs.readFileSync('config.json', { encoding: 'utf8', flag: 'r' }))
-
 const fileName = `${config.imgName}.${config.imgType}`
 
-console.log(config)
+const preLog =`
+- channel: ${config.channel }
+- imgRewardId: ${config.imgRewardId }
+- txtRewardId: ${config.txtRewardId }
+
+- txtFile: ${config.txtName}.txt
+- spaceCount: ${config.spaceCount }
+
+- imgFile: ${fileName}
+- resized: ${config.imgName}Resized.${config.imgType}
+- size: ${config.height } x ${config.width }
+- fit: ${config.fit }
+
+- fullLogging: ${config.fullLogging }
+`
+
+console.log(preLog.replaceAll(': ', ': \x1b[34m').replaceAll('\n', '\n\x1b[0m'))
+
+console.log(
+`Types for img Fit can be found there: \x1b[33m
+https://cdn.jsdelivr.net/gh/lovell/sharp@main/docs/image/api-resize-fit.svg \n\x1b[0m
+More info at: \x1b[33m
+https://github.com/MaxFrolikov/twitch-picture-catcher/blob/main/README.md \n\x1b[0m`
+)
 
 const client = new tmi.Client({
 	connection: {
@@ -19,12 +47,9 @@ const client = new tmi.Client({
 		reconnect: true,
 	},
 	channels: [config.channel],
-});
-;
+})
 
-client.connect();
-console.log('started')
-
+client.connect().then(() =>	console.log('started'));
 
 client.on('message', (channel, tags, message, self) => {
 
@@ -33,49 +58,44 @@ client.on('message', (channel, tags, message, self) => {
 	}
 
 	if(tags['custom-reward-id'] === config.imgRewardId) {
+		customLog('img', '', `${tags['display-name']}: ${message}`)
 		
-		console.log(`img | ${(new Date).toLocaleTimeString()} | ${tags['display-name']}: ${message}`)
-		
-		const last = message.split('.')[message.split('.').length - 1]
-		if(last !== 'png' && last !== 'jpg') {
-			console.log('Invalid Link')
-			return
-		}
-		
-		https.get(message, response => {
+		try {https.get(message, response => {
 			const file = fs.createWriteStream(fileName)
-			console.log('Creaing file')
 			response.pipe(file);
-			console.log('Writing file', fileName)
+
 			file.on('finish', () => {
 				file.close();
-				sharp(fileName)
+					sharp(fileName)
 					.resize({
 						height: config.height,
 						width: config.width,
-						fit: 'contain',
+						fit: config.fit,
 						background: {r: 0,g: 0,b: 0,alpha: 0}
 					})
 					.toFile(`${config.imgName}Resized.${config.imgType}`)
-					.then((console.log('Resized file Updated')))
+					.catch((err) => {
+						customLog('img', redLog, 'No img found')
+						return
+					})
+					.then(info => {if(info) customLog('img', greenLog, `Resiesd ${info.format} updated`)})
 			});
-		}).on('error', err => {
-			console.error(`Error downloading image: ${err.message}`);
-		});
-		return
+		})} catch (err) {
+			customLog('img', redLog, 'No Link')
+		}
 	}
 
 
 
 	if(tags['custom-reward-id'] === config.txtRewardId) {
-		console.log(`txt | ${(new Date).toLocaleTimeString()} | ${tags['display-name']}: ${message}`)
+		customLog('txt', '', `${tags['display-name']}: ${message}`)
 		
 		let textToWrite = message
 		for(let i = 0; i < config.spaceCount; i++){
 			textToWrite += ' '
 		}
 		fs.writeFileSync(`${config.txtName}.txt`, textToWrite)
-		console.log('Text file Updated')
+		customLog('txt', greenLog, 'Text file Updated')
 		
 		return
 	}
